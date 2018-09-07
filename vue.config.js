e73cf5c384a3d7a20a0017847df70233d926abfd
futengda <506036406@qq.com>
2018-09-07 20:51:01 +0800

    fix: 补充上页面C、D的webpack设置
    奇怪的是，即使之前没有写C、D两个页面的配置，错误依旧

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,18 @@ module.exports = {
       template: 'public/index.html',
       title: 'Page B',
       chunks: ['chunk-vendors', 'chunk-common', 'pageB']
+    },
+    pageC: {
+      entry: 'src/pages/pageC/app.js',
+      template: 'public/index.html',
+      title: 'Page C',
+      chunks: ['chunk-vendors', 'chunk-common', 'pageC']
+    },
+    pageD: {
+      entry: 'src/pages/pageD/app.js',
+      template: 'public/index.html',
+      title: 'Page D',
+      chunks: ['chunk-vendors', 'chunk-common', 'pageD']
     }
   }
 }
