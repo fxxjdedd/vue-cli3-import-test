7ab26c696cb6ec1c7ad75e7724acf99280177f39
futengda <506036406@qq.com>
2018-09-10 10:28:41 +0800

    add test eg

--- src/common/index.js
+++ src/common/index.js
@@ -1,7 +1,22 @@
-// test import from `pages` dir --> crash
 let filename = 'import-test.js'
+
+/* test import from `pages` dir --> crash */
 import(`@/pages/${filename}`)
 
-// test import from `non-pages` dir --> work well
-
+/* test import from `non-pages` dir --> work well */
 // import(`@/non-pages/${filename}`)
+
+/* test limit dir */
+// import(`@/pages/pageA/${filename}`);
+
+/* test include */
+// import(
+//   /* webpackInclude: /import-test.js$/ */
+//   `@/pages/${filename}`
+// );
+
+/* test exclude */
+// import(
+//   /* webpackExclude: /app/ */
+//   `@/pages/${filename}`
+// );
